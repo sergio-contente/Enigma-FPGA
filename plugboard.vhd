5ddf30e9d74aae335bc867f027a87ffe11047769
Sergio Contente <78960602+sergio-contente@users.noreply.github.com>
2022-11-23 04:07:42 -0300

    Update plugboard.vhd
    Co-authored-by: jonasgdm <48895777+jonasgdm@users.noreply.github.com>

--- plugboard.vhd
+++ plugboard.vhd
@@ -37,7 +37,7 @@ architecture plugboard_arch of plugboard is
 											"00110", "00111", "01000", "01001", "01010", "01011", 
 											"01100", "01101", "01110", "01111", "10000", "10001", 
 											"10010", "10011", "10100", "10101", "10110", "10111",
-											"11000", "11001"); -- Esse aqui eh o vetor que sofrer permutacoes com os ciclos de clock
+											"11000", "11001", "11010"); -- Esse aqui eh o vetor que sofrer permutacoes com os ciclos de clock
 	signal fixed_alphabet : alphabet := ("00000", "00001", "00010", "00011", "00100", "00101", 
 	"00110", "00111", "01000", "01001", "01010", "01011", 
 	"01100", "01101", "01110", "01111", "10000", "10001", 
