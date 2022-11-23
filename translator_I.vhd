882145e5f1e892f9813d1bf2f1aafa02b1906447
sergio-contente <sergio.mcontente@gmail.com>
2022-11-23 10:53:23 -0300

    ✨  Initializes translator logic

---
+++ translator_I.vhd
@@ -0,0 +1,18 @@
+------------------------------ENIGMA-------------------------------------
+-- Arquivo   : translator_I.vhd
+-- Projeto   : Enigma
+---------------------------------------------------------------------------
+-- Descricao :  Traducao do teclado:
+-- =========================================================================
+--							ABCDEFGHIJKLMNOPQRSTUVWXYZ
+--							EKMFLGDQVZNTOWYHXUSPAIBRCJ
+-- ========================================================================
+--							por combinar as letras de entrada.
+----------------------------------------------------------------------------
+-- Revisoes  :
+--     Data        Versao  Autor             						Descricao
+--     23/11/2022  1.0     Sergio Magalhaes Contente 		criacao
+------
+
+
+
